1c760ba2185ab9945eb24f62dedaaedc710b0467
lahiru2k14 <lahiru2k14@gmail.com>
2023-10-02 16:12:45 +0530

    open street map api url changed

--- app.js
+++ app.js
@@ -262,7 +262,7 @@ var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 const marker = L.marker([0, 0]).addTo(map);
